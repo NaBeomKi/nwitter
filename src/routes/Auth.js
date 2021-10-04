5e75e4d11819efbe7cfc5b0acece55769ffddb0a
beomki69 <beomki69@gmail.com>
2021-10-04 22:05:21 +0900

    Creating Account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { authService } from "firebase";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+
+  const handleChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    switch (name) {
+      case "email":
+        return setEmail(value);
+      case "password":
+        return setPassword(value);
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await createUserWithEmailAndPassword(
+          authService,
+          email,
+          password
+        );
+      } else {
+        data = await signInWithEmailAndPassword(authService, email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={handleChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={handleChange}
+        />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
